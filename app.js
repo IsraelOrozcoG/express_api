247e561a97985f55fdd9928429e4efd9c997a714
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2022-04-29 16:56:59 -0500

    Probando Endpoint en POSTMAN

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const express = requiere('express')
+const express = require('express')
 
 const app = express()
 
@@ -6,6 +6,19 @@ app.use(express.json())
 
 const port = 3000
 
+app.get('/v1/explorers', (req,res) => {
+    console.log(`Api Explorers GET ALL requests ${new Date()}`)
+
+    const explorer1 ={id: 1, name: "Carlo1"}
+    const explorer2 ={id: 1, name: "Carlo2"}
+    const explorer3 ={id: 1, name: "Carlo3"}
+    const explorer4 ={id: 1, name: "Carlo4"}
+    const explorer5 ={id: 1, name: "Carlo5"}
+    const explorers=[explorer1, explorer2, explorer3, explorer4]
+    res.status(200).json(explorers)
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
+
