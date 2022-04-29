cff313b83f72ccc4e22c5994eb22c061aa666904
IsraelOrozcoG@github <ing.orozcoisrael@gmail.com>
2022-04-29 16:18:50 -0500

    Codigo basico necesario para correr express

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = requiere('express')
+
+const app = express()
+
+app.use(express.json())
+
+const port = 3000
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
